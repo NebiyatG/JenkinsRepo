c37860859b5260e307f45bc0879d66c62a834d7b
Nebiyat <love.jesus.ng@gmail.com>
2023-12-08 18:16:52 -0800

    More Updates

--- Shell_Script_Exercise.sh
+++ Shell_Script_Exercise.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-$ chmod a+x ./Shell_Script_Exercise.sh
+$ chmod x ./Shell_Script_Exercise.sh
 
 
 
-sample1=+ 1sdwe5
-sample2=- swqwe43w
-sample3= 23354
-sample4= asdses
-sample5 = slooow2342r3sss
 
-egrep /"+ "|"- "/  shell-test.json
-egrep 
+
+# egrep /"+ "|"- "/  Script_TestCases
+
+echo $?
 
 
 
 egrep -i session auth.log
+
+echo $?
+exit 1
